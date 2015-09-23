c588fb8754aba6ac30578c9e27a5a0edba771727
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-09-24 02:26:37 +0300

    New sheet implemented, alpha sheet view

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-function fe_get_or($array, $key, $default_value) {
-    if array_key_exists($key, $array)) {
+define('TR_EXPENSE', 'exp');
+define('TR_INTERNAL', 'int');
+
+define('CUR_RUR', 'rur');
+define('CUR_USD', 'usd');
+
+
+function fe_get_or($array, $key, $default_value = "") {
+    if (array_key_exists($key, $array)) {
         return $array[$key];
     }
     return $default_value;
@@ -12,19 +19,93 @@ function fe_empty($value) {
 }
 
 function fe_new_sheet() {
+    global $PHP_SELF;
+    $sheet_id = rand().'-'.rand().'-'.rand();
+    ?>
+    <form method="post" action="<?php echo $PHP_SELF; ?>?action=new_sheet">
+        <input type="hidden" id="sheet_id" name="sheet_id" value="<?php echo $sheet_id; ?>" />
+        <input type="submit" value="Создать новый лист" />
+    </form><?php
 }
 
-function fe_edit_sheet($sheet_id) {
+function fe_print_transaction_input($transaction_id, $transaction) {
+    print_r($transaction_id);
+    print " &nbsp; ";
+    print_r($transaction);
+    print "<br />";
+}
 
+
+function fe_edit_sheet($sheet_id) {
+    echo "Идентификатор листа: <b>$sheet_id</b><br />";
+    $sheet_data = file_get_contents("data/$sheet_id.json");
+    $sheet_data = json_decode($sheet_data, true);
+    $members = $sheet_data["members"];
+    $transactions = fe_get_or($sheet_data, "transactions", array());
+    foreach ($members as $member_id => $member_name) {
+        print("$member_id $member_name<br/>");
+    }
+
+    foreach ($transactions as $transaction_id => $transaction) {
+        fe_print_transaction_input($transaction_id, $transaction);
+    }
+
+    print_r($sheet_data);
+    print_r($transactions);
 }
 
 $sheet_id = fe_get_or($_REQUEST, "sheet_id");
-$sheet_id = preg_replace("/^[0-9a-f-]/", "", $sheet_id);
+$sheet_id = preg_replace("/[^0-9a-f-]/", "", $sheet_id);
+$action = fe_get_or($_REQUEST, "action");
 
+if ($action == "new_sheet") {
+    $sheet_f = fopen("data/$sheet_id.json", "w");
+    $sheet_data = array();
+    $members = array();
+    $members["1"] = "one";
+    $members["2"] = "Two";
+    $members["3"] = "threE";
+    $sheet_data["members"] = $members;
+    $transactions = array();
+    $transactions["1"] = array(
+        "type"=>TR_EXPENSE,
+        "currency"=>CUR_RUR,
+        "charges"=>array(
+            "1"=>"1000",
+            "2"=>"500",
+        ),
+    );
+    $transactions["2"] = array(
+        "type"=>TR_EXPENSE,
+        "currency"=>CUR_USD,
+        "charges"=>array(
+            "1"=>"50",
+            "2"=>"100",
+        ),
+    );
+    $sheet_data["transactions"] = $transactions;
+
+
+    fwrite($sheet_f, json_encode($sheet_data));
+    fclose($sheet_f);
+    echo $PHP_SELF;
+    header("Location: /?sheet_id=$sheet_id");
+    exit();
+}
+
+?><!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Financial Equalizer</title>
+</head>
+<body><?php
 
 if (fe_empty($sheet_id)) {
     fe_new_sheet();
 } else {
     fe_edit_sheet($sheet_id);
 }
-
+?>
+</body>
+</html>
