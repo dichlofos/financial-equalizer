42e938218782832d00906228277255412f5489d3
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-10-05 22:35:04 +0300

    Added Yandex.Metrika counter

--- index.php
+++ index.php
@@ -377,6 +377,34 @@ if ($action == "new_sheet") {
     <script src="/static/bootstrap/js/bootstrap.js"></script>
 </head>
 <body>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter32864640 = new Ya.Metrika({
+                        id:32864640,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/32864640" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
     <div class="sheet-link">
         <?php
         if (fe_not_empty($sheet_id)) {
