97de28bc6aed16865a48a7f51aa8014903acb8a7
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2016-08-26 23:38:10 +0300

    Better

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-ssh -A dmvn.net bash -xe <<EOF
+hg push ssh://dmvn.net//home/mvel/work/financial-equalizer
+ssh dmvn.net bash -xe <<EOF
 cd $HOME/work/financial-equalizer
-hg pull
 hg up
 sudo ./local-install.sh
 EOF
