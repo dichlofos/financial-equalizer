d62e5db028b8c58461ec7a3beeaf7275559726df
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2016-09-07 12:59:47 +0300

    Modularize

---
+++ edit_sheet.php
@@ -0,0 +1,189 @@
+<?php
+
+require_once('transaction_input.php');
+
+
+function fe_edit_sheet($sheet_id) {
+    global $PHP_SELF;
+    $sheet_data = fe_load_sheet($sheet_id);
+
+    $members = fe_get_or($sheet_data, "members", array());
+    $transactions = fe_get_or($sheet_data, "transactions", array());
+    $exchange_rates = fe_get_or($sheet_data, "exchange_rates", array());
+
+    $result = fe_calc_sheet($sheet_data);
+    $deltas = $result["deltas"];
+    $member_sums = $result["member_sums"];
+    $all_transactions_sum = $result["all_transactions_sum"];
+    $bad_lambda_norm = $result["bad_lambda_norm"];
+    $avg_spendings = $result["avg_spendings"];
+
+    ?>
+
+    <div class="row">
+        <div class="col-md-4">
+            <form method="post" class="form-inline" action="<?php echo $PHP_SELF; ?>?action=add_member">
+                <div class="form-group">
+                    <input type="hidden" name="sheet_id" value="<?php echo $sheet_id; ?>" />
+                    <label for="member_name-input">Новый участник:&nbsp;</label>
+                    <input type="text" class="form-control" name="member_name" id="member_name-input" value="" placeholder="Иван Человеков" />
+                    <button type="submit" class="btn btn-primary">Добавить участника</button>
+                </div>
+            </form>
+        </div>
+
+        <div class="col-md-4">
+            <form method="post" class="form-inline" action="<?php echo $PHP_SELF; ?>?action=add_currency">
+                <div class="form-group">
+                <input type="hidden" name="sheet_id" value="<?php echo $sheet_id; ?>" />
+                <label for="currency-select">Новая валюта:&nbsp;</label>
+                <select class="form-control" name="currency" id="currency-select"><?php
+                $all_currencies = array(
+                    "EUR",
+                    "KGS",
+                    "RUR",
+                    "USD",
+                    "UZS",
+                );
+                foreach ($all_currencies as $curr) {
+                    echo "<option value=\"$curr\">$curr</option>\n";
+                }?>
+                </select>
+                <button type="submit" class="btn btn-primary">Добавить</button>
+                </div>
+            </form>
+        </div>
+
+        <div class="col-md-4">&nbsp;</div>
+    </div>
+
+    <form method="post" class="form-inline" action="<?php echo $PHP_SELF; ?>?action=add_transaction">
+        <div class="form-group">
+        <input type="hidden" name="sheet_id" value="<?php echo $sheet_id; ?>" />
+        <label for="description-input">Новая статья расходов:&nbsp;</label>
+        <input class="form-control" type="text" name="description" id="description-input" value="" placeholder="Скинулись на шаурму" />
+        <button type="submit" class="btn btn-primary">Добавить</button>
+        </div>
+    </form>
+    <?php
+
+    // See issue https://bitbucket.org/dichlofos/financial-equalizer/issues/13
+    $max_input_vars = (integer)(ini_get('max_input_vars'));
+    if (count($members) * count($transactions) * 4 > $max_input_vars) {
+        ?>
+        <div class="tip bg-warning">
+            Есть вероятность превышения количества входных переменных <tt>max_input_vars</tt>.
+            Пожалуйста, для корректной работы увеличьте лимит (по умолчанию значение равно&nbsp;1000).
+            Детали можно уточнить
+            в&nbsp;<a href="http://php.net/manual/en/info.configuration.php#ini.max-input-vars">документации</a>.
+        </div><?php
+    }
+    ?>
+
+    <form class="form-inline" method="post" action="<?php echo $PHP_SELF; ?>?action=update_sheet">
+        <input type="hidden" name="sheet_id" value="<?php echo $sheet_id; ?>" />
+        <label>Участники:</label><br/><?php
+        foreach ($members as $member_id => $member_name) {
+            echo "<div class=\"form-group member-list\"><label for=\"m$member_id-input\" style=\"width: 30px\">$member_id:&nbsp;</label>";
+            echo "<input class=\"form-control\" type=\"text\" name=\"m$member_id\" id=\"m$member_id-input\" value=\"$member_name\" /></div>\n";
+        }
+        ?><br/>
+        <label>Курсы валют:&nbsp;</label><br/><?php
+        foreach ($exchange_rates as $currency => $rate) {
+            echo "<div class=\"form-group member-list\"><label for=\"e$currency-input\" style=\"width: 40px\">$currency:&nbsp;</label>";
+            echo "<input class=\"form-control rate\" type=\"text\" name=\"e$currency\" id=\"e$currency-input\" value=\"$rate\" /></div>\n";
+        }
+
+        if (count($bad_lambda_norm)) {?>
+        <div class="tip bg-warning">
+            В ведомости присутствуют статьи расходов, которые ни на кого не были потрачены (выделены цветом)
+        </div><?php
+        }?>
+
+        <div id="transactions" class="transactions">
+        <table class="table table-condensed transactions">
+        <tr>
+        <th class="non-member">Статья расхода или сбора</th>
+        <th class="non-member">Валюта</th><?php
+        foreach ($members as $member_id => $member_name) {
+            echo "<th>$member_name</th>\n";
+        }
+        ?>
+        <th class="non-member">Сумма / Чел</th>
+        </tr>
+        <?php
+        foreach ($transactions as $transaction_id => $transaction) {
+            fe_print_transaction_input(
+                $members,
+                $transaction_id,
+                $transaction,
+                fe_get_or($deltas, $transaction_id, array()),
+                $exchange_rates,
+                fe_get_or($bad_lambda_norm, $transaction_id)
+            );
+        }
+        // Total
+        ?>
+        <tr>
+            <th>&nbsp;</th>
+            <th>&nbsp;</th>
+        <?php
+        foreach ($members as $member_id => $member_name) {
+            echo "<th>$member_name</th>\n";
+        }
+        ?>
+        <th>&nbsp;</th>
+        </tr>
+        <tr class="info">
+            <td>Итоговые расходы участника (RUR)</td>
+            <td>&nbsp;</td>
+        <?php
+        foreach ($members as $member_id => $member_name) {
+            $member_sum = $member_sums[$member_id];
+            $member_sum_rounded = (integer)($member_sum * 100) / 100;
+            echo "<td>$member_sum_rounded</td>\n ";
+        }
+        echo "<td><b>$all_transactions_sum</b> ($avg_spendings&nbsp;/&nbsp;<i>чел</i>)</td>\n";
+        ?>
+        </tr>
+        </table>
+        </div><!-- transactions scroller -->
+        <div>
+            <button type="submit" class="btn btn-primary">Сохранить</button>
+            Транзакций: <?php echo count($transactions); ?> шт.
+        </div>
+    </form>
+    <script>
+        $(function() {
+            var transactions = document.getElementById("transactions");
+            transactions.scrollTop = transactions.scrollHeight;
+        });
+    </script>
+
+    <div class="tip bg-info">
+    <?php
+    $tip_rnd = rand(0, 1);
+    echo "<b>Совет $tip_rnd</b>: ";
+    if ($tip_rnd == 0) {?>
+        Сборы в &laquo;кассу&raquo; удобно считать следующим образом. Всем, кто сдавал деньги, пишем их как обычные расходы,
+        а кассиру пишем суммарное количество этих собранных денег со знаком <b>минус</b>. Таким образом, сумма по данной транзакции
+        в правой колонке будет равна нулю. Точно так же можно учитывать, например, событие типа &laquo;Вася одолжил у Пети 500р на
+        пиво и мороженое&raquo;: Васе пишем&nbsp;-500, а Пете&nbsp;&#8212;&nbsp;500.<?php
+    } elseif ($tip_rnd == 1) {?>
+        Чтобы удалить валюту, сотрите её курс обмена и нажмите Enter. Чтобы удалить участника, сотрите его имя и нажмите Enter.
+        При удалении участника все суммы, связанные с ним, также будут удалены!
+        <?php
+    }?>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <form method="post" class="form-inline" action="/?action=clear_session">
+                <div class="form-group">
+                    <button type="submit" class="btn btn-warning">На главную</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <?php
+}
---
+++ transaction_input.php
@@ -0,0 +1,74 @@
+<?php
+
+function fe_currency_selector($currency, $id, $exchange_rates) {
+    echo "<select class=\"form-control input-sm currency-select\" name=\"$id\">";
+    foreach ($exchange_rates as $curr => $rate) {
+        $selected = ($curr == $currency) ? ' selected="selected" ' : '';
+        echo "<option value=\"$curr\"$selected>$curr</option>\n";
+    }
+    echo "</select>";
+}
+
+function fe_print_transaction_input(
+    $members,
+    $transaction_id,
+    $transaction,
+    $transaction_deltas,
+    $exchange_rates,
+    $bad_lambda_norm
+) {
+    $currency = fe_get_currency($transaction);
+    $description = fe_get_or($transaction, "description");
+
+    $bad_lambda_norm_class = $bad_lambda_norm ? "warning" : "";
+
+    echo "<tr class=\"$bad_lambda_norm_class\">";
+    echo "<td>";
+    echo "<input class=\"form-control input-sm transaction-description\" name=\"dtr${transaction_id}\" value=\"$description\" type=\"text\"
+        title=\"Товар или оказанная услуга\" placeholder=\"Трансфер из пункта А в пункт Б\" />";
+    echo "<td>";
+    fe_currency_selector($currency, "cur$transaction_id", $exchange_rates);
+    echo "</td>\n ";
+    $transaction_sum = 0;
+    $transaction_member_count = 0;
+
+    foreach ($members as $member_id => $member_name) {
+        $delta = $transaction_deltas[$member_id];
+        echo "<td>";
+        $charge_int = fe_get_charge($transaction, $member_id);
+        $transaction_sum += $charge_int;
+
+        $amount_class = "";
+        if ($charge_int > 0) {
+            $amount_class = "positive";
+        } elseif ($charge_int < 0) {
+            $amount_class = "negative";
+        }
+
+        echo "<input class=\"form-control input-sm amount $amount_class\"
+            name=\"tr${transaction_id}_${member_id}\"
+            value=\"$charge_int\"
+            type=\"text\"
+            title=\"Сколько потратил данный участник в указанной валюте\"
+        />";
+        $member_spent = fe_get_spent($transaction, $member_id);
+        $spent_class = "no-use";
+        if ($member_spent > 0.01 && $member_spent < 0.99) {
+            $spent_class = "lower-use";
+        } elseif ($member_spent >= 0.99 && $member_spent <= 1.01) {
+            $spent_class = "normal-use";
+        } elseif ($member_spent > 1.01) {
+            $spent_class = "high-use";
+        }
+
+        if ($member_spent > 0.01) {
+            ++$transaction_member_count;
+        }
+
+        echo "&nbsp;<input class=\"form-control input-sm spent $spent_class\" name=\"sp${transaction_id}_${member_id}\" value=\"$member_spent\" type=\"text\"
+            title=\"Коэффициент пользования данной услугой для данного участника\" />";
+        echo "</td>\n ";
+    }
+    echo "<td>$transaction_sum / $transaction_member_count</td>\n ";
+    echo "</tr>";
+}
