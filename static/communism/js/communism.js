e57ab84191e1e3262857ad8464860ad245910856
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-01-13 13:10:36 +0300

    Remove debug output

--- static/communism/js/communism.js
+++ static/communism/js/communism.js
@@ -2,11 +2,11 @@
 $(document).ready(function () {
     var total_width = $('#transactions').innerWidth();
 
-    console.log("total width: " + total_width);
+    // console.log("total width: " + total_width);
     total_width = _set_transaction_description_width(total_width);
-    total_width -= 70; // currency
-    total_width -= 80; // total sums
-    console.log("total width rest: " + total_width);
+    total_width -= 70; // currency, see CSS
+    total_width -= 80; // total stats, see CSS
+    // console.log("total width rest: " + total_width);
 
     _set_transaction_amount_width(total_width);
 
@@ -20,7 +20,7 @@ function _set_transaction_description_width(total_width) {
         // 2px is for border
         $(descriptions[i]).innerWidth(descr_width - 2);
     }
-    console.log("descr width: " + descr_width);
+    // console.log("descr width: " + descr_width);
 
     var inputs = $('input.transaction-description');
     for (i = 0; i < inputs.length; ++i) {
@@ -44,7 +44,7 @@ function _set_transaction_amount_width(total_width) {
         if (amount_width < 50)
             amount_width = 50;
     }
-    console.log("amount width: " + amount_width);
+    // console.log("amount width: " + amount_width);
     for (var i = 0; i < sums.length; ++i) {
         // 2px is for border
         $(sums[i]).width(amount_width - 2);
