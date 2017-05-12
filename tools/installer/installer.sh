d93596810f4a674554d7c8276203c922769012d5
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-05-13 02:24:35 +0300

    Fix directory creation

--- tools/installer/installer.sh
+++ tools/installer/installer.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 deploy_service() {
+
     service_name="$1"
     target_server="$2"
+
     work_dir="deploy/$1"
     production_workdir=$HOME/$work_dir
 
     # prepare repo on remote
     ssh $target_server bash -xe <<EOF
         if ! [ -e $production_workdir ] ; then
-            mkdir $production_workdir
+            mkdir -p $production_workdir
             cd $production_workdir
             hg init
         fi
