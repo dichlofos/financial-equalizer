642e6a6c90e5ee8ac4706234e810fe924bcb2db7
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-05-22 11:49:54 +0300

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,16 @@ my_self=$(readlink -f "$0")
 my_dir="$(dirname "$my_self")"
 
 set -e
+
 project_name="financial-equalizer"
+destination="$1"
+if [ -z "$destination" ] ; then
+    destination="$project_name"
+fi
 
 # bootstrap project into current directory
-hg clone ssh://hg@bitbucket.org/dichlofos/$project_name .
-hg clone ssh://hg@bitbucket.org/dichlofos/xengine site/engine
-hg clone ssh://hg@bitbucket.org/dichlofos/deploy-tools deploy-tools
+hg clone ssh://hg@bitbucket.org/dichlofos/$project_name $destination
+hg clone ssh://hg@bitbucket.org/dichlofos/xengine $destination/site/engine
+hg clone ssh://hg@bitbucket.org/dichlofos/deploy-tools $destination/deploy-tools
 
-echo "Bootstrapping $project_name with dependencies done"
+echo "Bootstrapping '$project_name' with dependencies done to '$destination'"
