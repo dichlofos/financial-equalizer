54bbfaf7ab57a5648031ed3599f1d7918a6e6ee2
Mikhail Veltishchev <mvel@yandex-team.ru>
2017-05-24 12:43:37 +0300

    Intellectual bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,15 @@ if [ -z "$destination" ] ; then
     destination="$project_name"
 fi
 
+common_repo_path="ssh://hg@bitbucket.org/dichlofos"
+if $(hostname) | grep -q dmvn ; then
+    # production host
+    common_repo_path="/srv/hg"
+fi
+
 # bootstrap project into current directory
-hg clone /srv/hg/$project_name $destination
-hg clone /srv/hg/xengine $destination/site/engine
-hg clone /srv/hg/deploy-tools $destination/deploy-tools
+hg clone $common_repo_path/$project_name $destination
+hg clone $common_repo_path/xengine $destination/site/engine
+hg clone $common_repo_path/deploy-tools $destination/deploy-tools
 
 echo "Bootstrapping '$project_name' with dependencies done to '$destination'"
