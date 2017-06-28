f31ae9703c552127ada5b887f15e378c0f9acea8
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-06-28 11:38:09 +0300

    Fix trash

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -5,22 +5,35 @@ my_dir="$(dirname "$my_self")"
 
 set -e
 
-if [ -x deploy-tools/.hg ] ; then
-    echo "Obtaining latest deploy-tools version"
-    ( cd $my_dir/deploy-tools && hg pull && hg update )
+xengine="site/xengine"
+
+function check_repo() {
+    [ -x $1/.hg ]
+}
+
+
+if echo $USER | grep -q mvel ; then
+    # TODO(mvel): rework this utter crap
+    if [ -x deploy-tools/.hg ] ; then
+        echo "Obtaining latest deploy-tools version"
+        ( cd $my_dir/deploy-tools && hg pull && hg update )
+    else
+        hg clone /home/mvel/work/deploy-tools deploy-tools
+    fi
 else
-    hg clone /home/mvel/work/deploy-tools deploy-tools
+    if ! [ -x deploy-tools/.hg ] || ! [ -x $xengine/.hg ] ; then
+        echo "Please checkout complete repo using bootstrap.sh"
+        exit 1
+    fi
 fi
+
+echo "Obtaining latest deploy-tools version"
+( cd $my_dir/deploy-tools && hg pull && hg update )
 
 . deploy-tools/installer/installer.sh
 
-engine="site/xengine"
+# from this momen we can use installer tools
+print_message "Obtaining latest xengine version"
+( cd $my_dir/$xengine && hg pull && hg update )
 
-if [ -x $engine/.hg ] ; then
-    echo "Obtaining latest xengine version"
-    ( cd $my_dir/$engine && hg pull && hg update )
-else
-    hg clone /home/mvel/work/xengine $engine
-fi
-
-echo "Updating dependencies done"
+print_message "Updating dependencies done"
