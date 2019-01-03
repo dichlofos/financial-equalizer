476e2f9ff3c1c83b715d529f4d424ab8948c74ae
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2019-01-04 01:14:26 +0300

    First prototype of database schema

--- nextgen/communism/communism/communism.py
+++ nextgen/communism/communism/communism.py
@@ -6,30 +6,93 @@ Main Financial Equalizer core
 
 import os
 import json
+import logging
 
 import flask as f
 
 import wtforms as wtf
 
+import flask_sqlalchemy as fsql
 
-app = f.Flask(__name__)  # create the application instance :)
-app.config.from_object(__name__)  # load config from this file , flaskr.py
 
+app = f.Flask(__name__)
+app.config.from_object(__name__)
 # Load default config and override config from an environment variable
 app.config.update(
     dict(
-        DATABASE=os.path.join(app.root_path, 'communism.db'),
         SECRET_KEY='development key',
         USERNAME='admin',
         PASSWORD='default',
+        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(app.root_path, 'communism.db'),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
     ),
 )
 
 app.config.from_envvar('COMMUNISM_SETTINGS', silent=True)
+db = fsql.SQLAlchemy(app)
+
+
+class Sheet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(256), nullable=False)
+
+    def __repr__(self):
+        return '<Sheet #{}: {}>'.format(self.id, self.description)
+
+    """
+    body = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False,
+        default=datetime.utcnow)
+    """
+
+
+class Member(db.Model):
+    """
+    A member of Sheet
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    display_name = db.Column(db.String(80), nullable=True)
+
+    sheet_id = db.Column(
+        db.Integer,
+        db.ForeignKey('sheet.id'),
+        nullable=False,
+    )
+    sheet = db.relationship(
+        'Sheet',
+        backref=db.backref('sheets', lazy=True),
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id'),
+        nullable=True,
+    )
+    user = db.relationship(
+        'User',
+        backref=db.backref('users', lazy=True),
+    )
+
+    def __repr__(self):
+        return '<Member #{}: {} {} {}>'.format(self.id, self.display_name, self.sheet_id, self.user_id)
+
+
+class User(db.Model):
+    # For registered users
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User #{}: {}, {}>'.format(self.id, self.user_name, self.email)
+
+
+class AddSheetForm(wtf.Form):
+    description = wtf.StringField('Название листа', [wtf.validators.Length(min=1, max=256)])
 
 
 class AddMemberForm(wtf.Form):
-    name = wtf.StringField('Имя участника', [wtf.validators.Length(min=1, max=25)])
+    display_name = wtf.StringField('Имя участника', [wtf.validators.Length(min=1, max=25)])
 
 
 class AddSpendingForm(wtf.Form):
@@ -46,7 +109,11 @@ class AddSpendingForm(wtf.Form):
 
 @app.route('/')
 def index():
-    return f.render_template('layout.html')
+    add_sheet_form = AddSheetForm(f.request.form)
+    return f.render_template(
+        'main.html',
+        add_sheet_form=add_sheet_form,
+    )
 
 
 @app.route('/sheets')
@@ -60,15 +127,19 @@ def sheet(sheet_id):
     add_member_form = AddMemberForm(f.request.form)
 
     add_spending_form = AddSpendingForm(f.request.form)
-    print(json.dumps(add_member_form.data, indent=4))
+
+    logging.info("add_member_form: %s", json.dumps(add_member_form.data, indent=4))
     if f.request.method == 'POST' and add_member_form.validate():
         print(add_member_form.name.data)
 
-        # user = User(form.username.data, form.email.data,
-        # form.password.data)
-        # db_session.add(user)
+        member = Member(add_member_form.display_name.data)
+        db.session.add(member)
         f.flash('Участник добавлен')
         return f.redirect(f.url_for('sheet', sheet_id=sheet_id))
+
+    if f.request.method == 'POST' and add_spending_form.validate():
+        print(add_spending_form.description.data)
+        print(add_spending_form.amount.data)
 
     return f.render_template(
         'sheet.html',
