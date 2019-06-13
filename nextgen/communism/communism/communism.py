17ac9b4c8ceecdac08671cb0c9b8ea81082a833f
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2019-06-13 09:31:04 +0300

    Spending partial membership is now correctly added and shown

--- nextgen/communism/communism/communism.py
+++ nextgen/communism/communism/communism.py
@@ -325,7 +325,7 @@ def sheet(sheet_id):
     ]
 
     add_spending_form.member_id.choices = member_choices
-    add_spm_form.member_id.choices = member_choices
+    add_spm_form.member_id.choices = [(-1, '(не выбран)')] + member_choices
 
     sheet_spm = SpendingPartialMembership.query.filter(
         SpendingPartialMembership.sheet_id == sheet_id
@@ -335,41 +335,34 @@ def sheet(sheet_id):
     for pm in sheet_spm:
         sheet_spm_by_spending[pm.spending_id].append(pm)
 
-    # logging.info('add_member_form: %s', json.dumps(add_member_form.data, indent=4))
-
-    if f.request.method == 'POST' and add_member_form.validate():
-        print(add_member_form.display_name.data)
-
-        member = Member(
-            sheet_id=sheet_id,
-            display_name=add_member_form.display_name.data,
-        )
-        db.session.add(member)
-        db.session.commit()
-        f.flash('Участник добавлен')
-        return f.redirect(f.url_for('sheet', sheet_id=sheet_id))
-
     if f.request.method == 'POST':
-        print(type(add_spending_form.data['description']))
+
+        logging.error("request values %s", f.request.values['spending_id'])
+
+        logging.error("desc type %s", type(add_spending_form.data['description']))
+        logging.error("add_spm_form type %s", type(add_spm_form.data))
         # print("\n".join(dir(add_spending_form.data)))
-        # for key in add_spending_form.data:
-        #    print(key, "value=", add_spending_form[key].value)
+        for key in add_spending_form.data:
+            logging.error("add_spending_form %s", key)
+
+        for key in add_spm_form.data:
+            logging.error("add_spm_form %s", key)
+        logging.error("add_spm_form %s", add_spm_form.weight.data)
+        logging.error("add_spm_form mid %s", add_spm_form.member_id.data)
+
+        if add_member_form.validate():
+            return _handle_add_member_form(add_member_form)
+
+        if add_spm_form.member_id.data != -1:
+            return _handle_add_spm_form(sheet_id, add_spm_form)
 
         if add_spending_form.validate():
-            spending = Spending(
-                sheet_id=sheet_id,
-                description=add_spending_form.description.data,
-                amount=add_spending_form.amount.data,
-                member_id=add_spending_form.member_id.data,
-            )
-            db.session.add(spending)
-            db.session.commit()
-            f.flash('Статья расходов добавлена')
-            return f.redirect(f.url_for('sheet', sheet_id=sheet_id))
-        else:
-            print('Validate add_spending_form FAILED')
-            f.flash('Чё-то вы не то пытаетесь сделать')
-            return f.redirect(f.url_for('sheet', sheet_id=sheet_id))
+            return _handle_add_spending_form(sheet_id, add_spending_form)
+
+        logging.error("BAD HANDLER")
+        # print('Validate add_spending_form FAILED')
+        f.flash('Чё-то вы не то пытаетесь сделать')
+        return f.redirect(f.url_for('sheet', sheet_id=sheet_id))
 
     if f.request.method == 'POST':
         if add_spm_form.validate():
@@ -391,3 +384,52 @@ def sheet(sheet_id):
         sheet_spendings=sheet_spendings,
         sheet_spm_by_spending=sheet_spm_by_spending,
     )
+
+
+def _handle_add_member_form(sheet_id, add_member_form):
+    """
+    `add_member_form` request handler
+    """
+    print(add_member_form.display_name.data)
+
+    member = Member(
+        sheet_id=sheet_id,
+        display_name=add_member_form.display_name.data,
+    )
+    db.session.add(member)
+    db.session.commit()
+    f.flash('Участник добавлен')
+    return f.redirect(f.url_for('sheet', sheet_id=sheet_id))
+
+
+def _handle_add_spm_form(sheet_id, add_spm_form):
+    """
+    `add_spm_form` request handler
+    """
+    logging.error("Adding SpendingPartialMembership")
+    # add_spm_form active
+    spm = SpendingPartialMembership(
+        sheet_id=sheet_id,
+        spending_id=f.request.values['spending_id'],
+        weight=add_spm_form.weight.data,
+        member_id=add_spm_form.member_id.data,
+    )
+    db.session.add(spm)
+    db.session.commit()
+    f.flash('Неполное участие добавлено')
+    return f.redirect(f.url_for('sheet', sheet_id=sheet_id))
+
+
+def _handle_add_spending_form(sheet_id, add_spending_form):
+    spending = Spending(
+        sheet_id=sheet_id,
+        description=add_spending_form.description.data,
+        amount=add_spending_form.amount.data,
+        member_id=add_spending_form.member_id.data,
+    )
+    db.session.add(spending)
+    db.session.commit()
+    f.flash('Статья расходов добавлена')
+    return f.redirect(f.url_for('sheet', sheet_id=sheet_id))
+
+
