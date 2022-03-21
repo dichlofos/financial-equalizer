3390c2e4c70c89f92676c11bdb05250dab99be1b
Mikhail Veltishchev <mvel@yandex-team.ru>
2022-03-21 23:55:36 +0300

    Switch to `git`

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,21 +11,21 @@ if [ -z "$destination" ] ; then
     destination="$project_name"
 fi
 
-common_repo_path="ssh://hg@bitbucket.org/dichlofos"
+common_repo_path="git@github.com:dichlofos/"
 if echo $(hostname) | grep -q dmvn ; then
     # production host
-    common_repo_path="/srv/hg"
+    common_repo_path="/srv/git"
 fi
 
 # bootstrap project into current directory
-hg clone $common_repo_path/$project_name $destination
-hg clone $common_repo_path/xengine $destination/site/xengine
-hg clone $common_repo_path/deploy-tools $destination/deploy-tools
+git clone $common_repo_path/$project_name.git $destination
+git clone $common_repo_path/xengine.git $destination/site/xengine
+git clone $common_repo_path/deploy-tools.git $destination/deploy-tools
 
 . $destination/deploy-tools/installer/installer.sh
 
-fix_hgrc $destination $project_name
-fix_hgrc $destination/site/xengine xengine
-fix_hgrc $destination/deploy-tools deploy-tools
+# fix_hgrc $destination $project_name
+# fix_hgrc $destination/site/xengine xengine
+# fix_hgrc $destination/deploy-tools deploy-tools
 
 echo "Bootstrapping '$project_name' with dependencies done to '$destination'"
